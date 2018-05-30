b282fab4a24d6bc04babecf6dbbe674adec8d910
Valery Harasiuk <Valery_Harasiuk@epam.com>
2018-05-30 18:54:37 +0300

    Delete email

--- email.poject.gradle/src/main/java/SendMail.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import Service.Send;
-
-public class SendMail {
-    public static void main(String[] args) throws IOException {
-        Send send = new Send();
-        send.sendMessage(args);
-    }
-}
